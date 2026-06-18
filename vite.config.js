8fe2acd2fac6b5fdaf55b3527629d671083dc358
NguyennDangg <nguynhai.dangg@gmail.com>
2026-06-18 23:04:05 +0700

    chore: setup master gitignore from template

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  root: './',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        day01: resolve(__dirname, 'day-01-rotating-cube/index.html'),
+        day01Project: resolve(__dirname, 'day01-project/index.html'), // Added this line!
+        day02: resolve(__dirname, 'day-02-orbitctrl-datgui/index.html'),
+        day03: resolve(__dirname, 'day-03-lighting-materials/index.html'),
+        day04: resolve(__dirname, 'day-04-gltf-hdr/index.html')
+      }
+    }
+  }
+});
